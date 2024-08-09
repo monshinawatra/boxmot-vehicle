59538dfbefb8b4c7fcd216112e37c9dc7e8b49f2
monshinawatra <monshinawatra@gmail.com>
2024-08-09 17:24:30 +0700

    Resize fixing

--- boxmot/appearance/backends/base_backend.py
+++ boxmot/appearance/backends/base_backend.py
@@ -9,7 +9,7 @@ from boxmot.appearance.reid_model_factory import (
     get_model_url,
     build_model,
     get_nr_classes,
-    show_downloadable_models
+    show_downloadable_models,
 )
 from boxmot.utils.checks import RequirementsChecker
 
@@ -37,13 +37,13 @@ class BaseModelBackend:
     def get_crops(self, xyxys, img):
         crops = []
         h, w = img.shape[:2]
-        resize_dims = (128, 256)
+        resize_dims = (256, 256)
         interpolation_method = cv2.INTER_LINEAR
         mean_array = np.array([0.485, 0.456, 0.406])
         std_array = np.array([0.229, 0.224, 0.225])
         # dets are of different sizes so batch preprocessing is not possible
         for box in xyxys:
-            x1, y1, x2, y2 = box.astype('int')
+            x1, y1, x2, y2 = box.astype("int")
             x1 = max(0, x1)
             y1 = max(0, y1)
             x2 = min(w - 1, x2)
@@ -92,10 +92,7 @@ class BaseModelBackend:
         # warmup model by running inference once
         if self.device.type != "cpu":
             im = np.random.randint(0, 255, *imgsz, dtype=np.uint8)
-            crops = self.get_crops(xyxys=np.array(
-                [[0, 0, 64, 64], [0, 0, 128, 128]]),
-                img=im
-            )
+            crops = self.get_crops(xyxys=np.array([[0, 0, 64, 64], [0, 0, 128, 128]]), img=im)
             crops = self.inference_preprocess(crops)
             self.forward(crops)  # warmup
 
@@ -117,12 +114,10 @@ class BaseModelBackend:
             elif isinstance(x, np.ndarray):
                 x = np.transpose(x, (0, 2, 3, 1))  # Convert from NCHW to NHWC
         return x
-    
+
     def inference_postprocess(self, features):
         if isinstance(features, (list, tuple)):
-            return (
-                self.to_numpy(features[0]) if len(features) == 1 else [self.to_numpy(x) for x in features]
-            )
+            return self.to_numpy(features[0]) if len(features) == 1 else [self.to_numpy(x) for x in features]
         else:
             return self.to_numpy(features)
 
@@ -134,15 +129,12 @@ class BaseModelBackend:
     def load_model(self, w):
         raise NotImplementedError("This method should be implemented by subclasses.")
 
-
     def download_model(self, w):
         if w.suffix == ".pt":
             model_url = get_model_url(w)
             if not w.exists() and model_url is not None:
                 gdown.download(model_url, str(w), quiet=False)
             elif not w.exists():
-                LOGGER.error(
-                    f"No URL associated with the chosen StrongSORT weights ({w}). Choose between:"
-                )
+                LOGGER.error(f"No URL associated with the chosen StrongSORT weights ({w}). Choose between:")
                 show_downloadable_models()
                 exit()
